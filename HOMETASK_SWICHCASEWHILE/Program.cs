c6b0d8f118a159b79296443ff2ee50ba708ec912
nihadaslanli <nihadsasl@code.edu.az>
2024-02-18 22:58:54 +0400

    Add project files.

---
+++ HOMETASK_SWICHCASEWHILE/Program.cs
@@ -0,0 +1,97 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+#region task3
+//Console.Write("qapinin enin daxil edin:");
+
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.Write("qapinin uzunluqun daxil edin:");
+
+//int b = Convert.ToInt32(Console.ReadLine());
+//Console.Write("skafin enin daxil edin:");
+
+//int c = Convert.ToInt32(Console.ReadLine());
+//Console.Write("skafin hundurluyu daxil edin:");
+
+//int d = Convert.ToInt32(Console.ReadLine());
+
+//Console.Write("skafin derinliyi daxil edin:");
+
+//int e = Convert.ToInt32(Console.ReadLine());
+
+//if (a > c && b > d)
+//{
+// Console.WriteLine("skaf qapidan kecir");
+//  if (a > e && b > d)
+//    {
+//Console.WriteLine("skaf qapidan kecir");
+//      if (a > c && b > e)
+//        {
+//Console.WriteLine("skaf qapidan kecir");
+      
+//else
+//    Console.WriteLine("skaf qapidan kecmir");
+
+
+
+
+
+
+
+
+
+
+#endregion
+
+
+#region task4
+//CorrectFormat:
+//Console.Write("istenilen musbet ededi daxil edin:");
+
+//int n = Convert.ToInt32(Console.ReadLine());
+//bool isLoop = true;
+//while (isLoop)
+
+
+//{
+//    if (n <= 0)
+//    {
+//        Console.WriteLine("duzgun ededi daxil et");
+//        goto CorrectFormat;
+
+//    }
+
+//    else
+//    {
+
+
+
+
+//        Console.WriteLine(n);
+//        isLoop = false;
+//    }
+
+//}
+
+
+
+
+#endregion
+
+
+#region task5
+//Console.Write("ededi daxil edin:");
+
+//int a = Convert.ToInt32(Console.ReadLine());
+//int count = 0;
+
+//while (a > 0 ) 
+//{
+
+//    a /= 10;
+//    count++;
+//}
+
+
+//Console.WriteLine(count);
+#endregion
