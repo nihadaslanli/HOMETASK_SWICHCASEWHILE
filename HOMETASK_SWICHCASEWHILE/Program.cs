6583f769e9d239118ed6dbb4b3c74a2a73be978d
nihadaslanli <nihadsasl@code.edu.az>
2024-02-20 00:21:16 +0400

    add anything

--- HOMETASK_SWICHCASEWHILE/Program.cs
+++ HOMETASK_SWICHCASEWHILE/Program.cs
@@ -2,39 +2,44 @@
 // See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 #region task3
-//Console.Write("qapinin enin daxil edin:");
+Console.Write("qapinin enin daxil edin:");
 
-//int a = Convert.ToInt32(Console.ReadLine());
-//Console.Write("qapinin uzunluqun daxil edin:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("qapinin uzunluqun daxil edin:");
 
-//int b = Convert.ToInt32(Console.ReadLine());
-//Console.Write("skafin enin daxil edin:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("skafin enin daxil edin:");
 
-//int c = Convert.ToInt32(Console.ReadLine());
-//Console.Write("skafin hundurluyu daxil edin:");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.Write("skafin hundurluyu daxil edin:");
 
-//int d = Convert.ToInt32(Console.ReadLine());
+int d = Convert.ToInt32(Console.ReadLine());
 
-//Console.Write("skafin derinliyi daxil edin:");
+Console.Write("skafin derinliyi daxil edin:");
 
-//int e = Convert.ToInt32(Console.ReadLine());
+int e = Convert.ToInt32(Console.ReadLine());
 
-//if (a > c && b > d)
-//{
-// Console.WriteLine("skaf qapidan kecir");
-//  if (a > e && b > d)
-//    {
-//Console.WriteLine("skaf qapidan kecir");
-//      if (a > c && b > e)
-//        {
-//Console.WriteLine("skaf qapidan kecir");
+if (a > c && b > d)
+{
+    Console.WriteLine("skaf qapidan kecir");
+    break;
+}
+    if (a > e && b > d)
+    {
+        Console.WriteLine("skaf qapidan kecir");
+        break;}
+if (a > c && b > e)
+{
+    Console.WriteLine("skaf qapidan kecir");
+    break;
+}
+else
+{
+    Console.WriteLine("skaf qapidan kecmir");
 
-//else
-//    Console.WriteLine("skaf qapidan kecmir");
-
-
-
-
+}
+        
+    
 
 
 
